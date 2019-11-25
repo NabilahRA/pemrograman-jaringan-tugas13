4790c3fd1a710befaed9b43cd86938efa8a0a34e
NabilahRA <nabilahroisul811@gmail.com>
2019-11-25 15:19:07 +0700

    tugas13 pemrograman jaringan

---
+++ src/Tugas3/PemrogramanJaringanTugas13.java
@@ -0,0 +1,52 @@
+/*
+ * pemrograman-jaringan-tugas13
+
+ * Copyright (c) 2019
+ * All rights reserved.
+ * Written by od3ng created on Nov 19, 2019 10:08:35 AM
+ * Blog    : sinaungoding.com
+ * Email   : lepengdados@gmail.com
+ * Github  : 0d3ng
+ * Hp      : 085878554150
+ */
+package Tugas3;
+import java.awt.AWTException;
+import java.awt.Rectangle;
+import java.awt.Robot;
+import java.awt.image.BufferedImage;
+import javax.swing.JFrame;
+/**
+ *
+ * @author od3ng
+ */
+public class PemrogramanJaringanTugas13 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        BufferedImage capture = null;
+        int width = 400;
+        int height = 400;
+        try{
+            Robot robo = new Robot();
+            //delay untuk mengatur layar
+            robo.delay(1000);
+            
+            capture = robo.createScreenCapture(new Rectangle(100,100,width,height));
+            if(capture != null){
+                Canvas panel = new Canvas(capture);
+                JFrame frame = new JFrame("Test Capture");
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.add(panel);
+                frame.pack();
+                frame.setLocationRelativeTo(null);
+                frame.setVisible(true);
+            }
+        } catch (AWTException ex){
+            
+        }
+        
+    }
+    
+}
---
+++ src/Tugas3/Robots.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Tugas3;
+import java.awt.AWTException;
+import java.awt.Robot;
+/**
+ *
+ * @author asus
+ */
+public class Robots {
+    public static void main(String[] args) {
+        try{
+            Robot robo = new Robot();
+        } catch (AWTException ex){
+            
+        }
+    }
+    
+}
